92bd09bcb1090ceeda145f0fc4083e81112be8c6
Gasparh2001 <gasparh2001@gmail.com>
2023-12-12 11:04:18 +0100

    panel and initial script

---
+++ Assets/Scripts/ChangeColorUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorUI : MonoBehaviour
+{
+    public int snakeRedHead = 0;
+    public int snakeGreenHead = 1;
+    public int snakeBlueHead = 2;
+
+    public int snakeRedBody = 0;
+    public int snakeGreenBody = 1;
+    public int snakeBlueBody = 2;
+
+    public int[] snakeColorNumber = { 0, 1, 2 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
